34e5db66d7cde55be56c3581728170c2a3b34976
bo.jiang <bo.jiang@onebank.com.cn>
2017-08-14 07:33:29 +0800

    切换数据库到mysql

--- sg/settings.py
+++ sg/settings.py
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'sg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sg',
+        'USER': 'root',
+        'PASSWORD': '1jcsxdL1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
 
